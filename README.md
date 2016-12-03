6cb3d2464ad625126ca23eed294b74af70bed4b0
Gustav Tiger <gustav@tiger.name>
2016-12-03 10:46:53 +0100

    Add performance switch

--- README.md
+++ README.md
@@ -3,16 +3,22 @@
 Here are my solutions built in Python 3. The structure is as follows:
 
 * `aoc.py` - The CLI for the project. Read below how to use it.
-* `day_X.py` - The soluion to day X
-* `day_X.txt` - The input to day X
+* `day_X.py` - The solution to day `X`
+* `day_X.txt` - The input to day `X`
 
 ## How to use the CLI
 
-* Run `./aoc.py --help` for help
-  * The flag `--day X [Y [Z ...]]` selects which day(s) to run. Default is 
-    day 1 to 24.
-  * The flag `--stdin` reads problem input from stdin instead of the default
-    `day_X.txt`.
+```
+usage: aoc.py [-h] [--day DAY [DAY ...]] [--perf] [--stdin]
+
+Advent of Code 2016
+
+optional arguments:
+  -h, --help           show this help message and exit
+  --day DAY [DAY ...]  select days (default: 1-25)
+  --perf               enable performance measuring (default: off)
+  --stdin              read input from stdin (default: off)
+```
 
 ### Example usage
 
--- aoc.py
+++ aoc.py
@@ -2,14 +2,17 @@
 
 import argparse
 import importlib
+import statistics
 import sys
 import time
+
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="Advent of Code 2016")
-    parser.add_argument("--day", type=int, dest="days", nargs="+", default=range(1, 25))
-    parser.add_argument("--stdin", dest='stdin', action='store_true', default=False)
+    parser.add_argument("--day", type=int, dest="days", metavar="DAY", nargs="+", default=range(1, 26), help="select days (default: 1-25)")
+    parser.add_argument("--perf", dest='perf', action='store_true', default=False, help="enable performance measuring (default: off)")
+    parser.add_argument("--stdin", dest='stdin', action='store_true', default=False, help="read input from stdin (default: off)")
     args = parser.parse_args()
 
     print("Advent of Code 2016")
@@ -27,10 +30,18 @@ if __name__ == "__main__":
             for step in range(1, 3):
                 function = "step{}".format(step)
                 if hasattr(problem, function) and callable(getattr(problem, function)):
-                    start_time = time.perf_counter()
-                    result = getattr(problem, function)()
-                    end_time = time.perf_counter()
-                    print("Step {}: {} (took {:.2f} µs)".format(step, result, (end_time - start_time) * 1000000))
+                    if args.perf:
+                        perfs = []
+                        for i in range(1000):
+                            probfunc = getattr(problem, function)
+                            start_time = time.perf_counter()
+                            result = probfunc()
+                            end_time = time.perf_counter()
+                            perfs.append((end_time - start_time) * 1000000)
+                        avg = statistics.mean(perfs)
+                        print("Step {}: {} (avg {:.2f} µs)".format(step, result, avg))
+                    else:
+                        print("Step {}: {}".format(step, getattr(problem, function)()))
             print()
         except ImportError as e:
             print("Day", day, "is not implemented yet")
